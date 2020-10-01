b9136963c84aa39172c99a162ed666ebb3000bb6
aprilhoward <apehow111@gmail.com>
2020-09-30 20:24:45 -0400

    Initial commit

---
+++ src/SquareStar.java
@@ -0,0 +1,25 @@
+public class SquareStar {
+
+    public static int printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println();
+            System.out.println();
+            for (int i = 0; i < number; i++) {
+                for (int j = 0; j < number; j++) {
+
+
+
+                    if (i == 0 || (i == number - 1) || j == 0 || (j == number - 1) || (i == j) || ((j + 1) == number - (i + 1) + 1)) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+        return number;
+    }
+}
